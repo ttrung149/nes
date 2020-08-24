83f8ba7542690d509d8713a10eb53ad7417bd624
Trung Truong <ttruong91498@gmail.com>
2020-08-24 16:34:39 -0400

    Add basic memory configurations

---
+++ src/mem.h
@@ -0,0 +1,14 @@
+#ifndef MEM_H_
+#define MEM_H_
+
+#define RESET_PC 0xFFFC
+#define RESET_STKP 0xFD 
+#define BASE_STKP 0x0100
+
+#define IRQ_PC 0xFFFE
+#define NMI_PC 0xFFFA
+
+#define SYSTEM_RAM_ADDR_LOWER 0x0000
+#define SYSTEM_RAM_ADDR_UPPER 0x1FFF
+
+#endif
